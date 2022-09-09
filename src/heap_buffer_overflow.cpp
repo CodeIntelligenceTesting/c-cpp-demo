45f8eafade3e8066f280e2de6c989ac558a98e1b
Niklas Henrich <niklas.henrich@code-intelligence.com>
2022-09-09 19:10:09 +0200

    Remove base64 string constant

--- src/heap_buffer_overflow.cpp
+++ src/heap_buffer_overflow.cpp
@@ -1,12 +1,9 @@
 #include "heap_buffer_overflow.h"
-#include <base64.h>
 #include <cstring>
 #include <cstdio>
 
-const char* SECRET = "RlVaWklORw=="; // base64("FUZZING")
-
 void heap_buffer_overflow(int a, int b, std::string c) {
-  if (a > 100 && b < 100 && c == SECRET) {
+  if (a > 100 && b < 100 && c == "FUZZING") {
           // Trigger a heap buffer overflow
           char *s = (char *)malloc(1);
           strcpy(s, "too long");
